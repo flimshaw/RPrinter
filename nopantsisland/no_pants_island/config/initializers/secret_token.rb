45e0fe433e54cc3af7ea5db7516dfcc35406253f
Ashley Holtgraver <aholtgraver@gmail.com>
2012-02-12 21:17:20 -0500

    HERE YOU GO CHARLIE

---
+++ nopantsisland/no_pants_island/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NoPantsIsland::Application.config.secret_token = 'a3e22bbe0dd39a0903aa8bffa34d4dad03666e833fc995467d68c0d9a3a08ec476de384092cda4ded807df10a31e9abe33b006b9e3120ca86030b1efdee190e8'
