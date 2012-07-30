97ddd6a45f47e2ffda688b5cb2d0dcc02643aa96
Charlie Hoey <choey@barbariangroup.com>
2012-07-29 20:22:24 -0400

    updated printer driver to begin processing HTML tags as printer commands

--- RPrinter/printer.py
+++ RPrinter/printer.py
@@ -12,6 +12,7 @@ VERSION = "0.25 ALPHA"
 
 DEBUG = True
 
+# bytecodes for various printer commands, perhaps soon to be legacy
 EMPHASIZE = 8
 DOUBLEHEIGHT = 16
 DOUBLEWIDTH = 32
@@ -25,6 +26,17 @@ class RPrinter:
     printSettings = 0b00000000
     textwrap = textwrap.TextWrapper()
 
+    # Font style byte codes
+
+    # 0 - Font size mode - 12x24 / 16x16 (?)
+    # 0 - *
+    # 0 - *
+    # 0 - Bold
+    # |
+    # 0 - Double Height
+    # 0 - Double Width
+    # 0 - *
+    # 0 - Underline Mode (?)
     printerStyles = {
         'strong': 8,
         'h1': 48,
@@ -63,22 +75,38 @@ class RPrinter:
             print str(data)
 
     def setTypeMode(self, tag):
-        if tag in printerStyles:
-            self.printSettings = self.printSettings ^ mode
+        print 'th'
+        if tag in self.printerStyles:
+            self.printSettings = self.printSettings ^ printerStyles[tag]
             if DEBUG == False:
                 self.send("\x1b\x21" + chr(self.printSettings))
             else:
-                print tag
+                print "Settings: " + chr(self.printSettings)
 
     # buffers the given string and prints it out to the printer
     def printStr(self, msg):
 
         html = markdown.markdown(msg)
+
         p = re.compile(r'(</?[^>]+>)')
-        commandList = p.split(html)
+
+        try:
+            commandList = p.split(html)
+        except NoneType:
+            raise
+
+        print commandList
+
         for cmd in commandList:
-            if cmd != '' or cmd != '\n':
-                print cmd
+
+            if cmd == '':
+                continue
+
+            if cmd[0] == "<" and cmd[-1] == ">":
+                print re.match(r'</?([^>]+)>', cmd).group()
+                #self.setTypeMode()
+            else:
+                print "cmd: " + cmd
 
     def printChar(self, msg):
         self.send(msg)
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+source env/bin/activate
+python twitterFeed.py
